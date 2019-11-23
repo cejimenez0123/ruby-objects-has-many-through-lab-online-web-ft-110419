c00868e66871a5b2d6d2759aef0f318cf8491351
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-23 20:01:06 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Artist
+attr_reader :name
+@@all = []
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs
+    Song.all.find_all {|song| song.artist == self}
+  end
+  def new_song(name,genre)
+    Song.new(name,self,genre)
+  end
+  def genres
+     genre_array = songs.map { |song|
+      song.genre
+
+    }
+    genre_array
+  end
+end
